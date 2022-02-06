37cc532edd3bb60b79fc0d9c322285584483f48b
Hsin <hsin@impactinstitute.com>
2022-02-06 03:35:08 +0100

    auth0 login logout

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -41,8 +41,116 @@ let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
               Todos = model.Todos @ [ todo ] },
         Cmd.none
 
+open Fable.Core
+open Fable.Auth0.React
 open Feliz
 open Feliz.Bulma
+open Feliz.Bulma.Operators
+
+let auth0App (children: seq<ReactElement>) =
+    let opts =
+        unbox<Auth0ProviderOptions>
+            {| domain = "dev-nik3xlx8.us.auth0.com"
+               clientId = "t81FqAoSB0LQn0tgCDSNG0u7z34oC0SW"
+               redirectUri = Browser.Dom.window.location.origin
+               audience = "https://dev-nik3xlx8.us.auth0.com/api/v2/"
+               scope = "read:current_user update:current_user_metadata" |}
+    Auth0Provider opts children
+
+
+[<ReactComponent>]
+let AuthenticationBox () =
+    let ctxAuth0 = useAuth0 ()
+
+    // correspond to authentication method `loginWithRedirect()` in JS
+    let handleLoginWithRedirect _ =
+        let opts = unbox<RedirectLoginOptions> null
+        ctxAuth0.loginWithRedirect opts
+        |> Async.AwaitPromise
+        |> Async.StartImmediate
+
+    // correspond to authentication method `logout({ returnTo: window.location.href })` in JS
+    let handleLogoutWithRedirect _ =
+        let returnTo = Browser.Dom.window.location.href
+        let opts = unbox<LogoutOptions> {| returnTo = returnTo |}
+        ctxAuth0.logout opts
+
+    let loginButton =
+        Bulma.button.button [
+            color.isPrimary
+            prop.onClick handleLoginWithRedirect
+            prop.text "Login"
+        ]
+
+    let logoutButton =
+        Bulma.button.button [
+            color.isPrimary
+            prop.onClick handleLogoutWithRedirect
+            prop.children [
+                Html.span "Logout"
+                Bulma.icon [ Html.i [ prop.className "fas fa-sign-out-alt" ] ]
+            ]
+        ]
+
+    match ctxAuth0.isLoading, ctxAuth0.user with
+    | _, None ->
+        // if not signed in, show login button
+        loginButton
+    | true, _ ->
+        Bulma.icon [
+            icon.isLarge
+            prop.children [
+                Html.i [ prop.className "fas fa-spinner" ]
+            ]
+        ]
+    | false, Some (user: User) ->
+        // if signed in, show user profile and logout button
+        let username, picture, usersub =
+            $"{user.name}", $"{user.picture}", $"{user.sub}"
+
+        let userProfile =
+            Bulma.level [
+                color.hasBackgroundPrimary
+                prop.style [ style.padding (length.em 0.5) ]
+                prop.children [
+                    Bulma.levelItem [
+                        Bulma.image [
+                            Bulma.image.isRounded
+                            ++ Bulma.image.is48x48
+                            prop.children [
+                                Html.img [
+                                    prop.style [ style.maxHeight.unset ]
+                                    prop.alt username
+                                    prop.src picture
+                                ]
+                            ]
+                        ]
+                    ]
+                    Bulma.levelItem [
+                        Bulma.title.p [
+                            Bulma.title.is5
+                            prop.text username
+                        ]
+                    ]
+                ]
+            ]
+
+        Bulma.level [
+            Bulma.levelLeft [
+                Bulma.levelItem [
+                    userProfile
+                ]
+                Bulma.levelItem [
+                    Bulma.field.div [
+                        Bulma.field.hasAddons
+                        prop.style [ style.marginLeft (length.em 1) ]
+                        prop.children [
+                            Bulma.control.p [ logoutButton ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
 
 let navBrand =
     Bulma.navbarBrand.div [
@@ -55,6 +163,9 @@ let navBrand =
                     prop.alt "Logo"
                 ]
             ]
+        ]
+        Bulma.navbarItem.div [
+            AuthenticationBox ()
         ]
     ]
 
@@ -91,32 +202,35 @@ let containerBox (model: Model) (dispatch: Msg -> unit) =
         ]
     ]
 
+
 let view (model: Model) (dispatch: Msg -> unit) =
-    Bulma.hero [
-        hero.isFullHeight
-        color.isPrimary
-        prop.style [
-            style.backgroundSize "cover"
-            style.backgroundImageUrl "https://unsplash.it/1200/900?random"
-            style.backgroundPosition "no-repeat center center fixed"
-        ]
-        prop.children [
-            Bulma.heroHead [
-                Bulma.navbar [
-                    Bulma.container [ navBrand ]
-                ]
+    auth0App [
+        Bulma.hero [
+            hero.isFullHeight
+            color.isPrimary
+            prop.style [
+                style.backgroundSize "cover"
+                style.backgroundImageUrl "https://unsplash.it/1200/900?random"
+                style.backgroundPosition "no-repeat center center fixed"
             ]
-            Bulma.heroBody [
-                Bulma.container [
-                    Bulma.column [
-                        column.is6
-                        column.isOffset3
-                        prop.children [
-                            Bulma.title [
-                                text.hasTextCentered
-                                prop.text "safe_auth0"
+            prop.children [
+                Bulma.heroHead [
+                    Bulma.navbar [
+                        Bulma.container [ navBrand ]
+                    ]
+                ]
+                Bulma.heroBody [
+                    Bulma.container [
+                        Bulma.column [
+                            column.is6
+                            column.isOffset3
+                            prop.children [
+                                Bulma.title [
+                                    text.hasTextCentered
+                                    prop.text "safe_auth0"
+                                ]
+                                containerBox model dispatch
                             ]
-                            containerBox model dispatch
                         ]
                     ]
                 ]
