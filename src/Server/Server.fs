a95452dc9fbc629f8baf8499f6826feb730107e6
Hsin <hsin@impactinstitute.com>
2022-02-10 02:43:33 +0100

    Revert "server setup using giraffe"
    This reverts commit c83df73df51162a67de6ddf0a92411c2422d3c1e.

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -1,25 +1,62 @@
 module Server
 
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Hosting
+open Fable.Remoting.Server
+open Fable.Remoting.Giraffe
+open Saturn
 
 open Shared
+
+type Storage() =
+    let todos = ResizeArray<_>()
+
+    member __.GetTodos() = List.ofSeq todos
+
+    member __.AddTodo(todo: Todo) =
+        if Todo.isValid todo.Description then
+            todos.Add todo
+            Ok()
+        else
+            Error "Invalid todo"
+
+let storage = Storage()
+
+storage.AddTodo(Todo.create "Create new SAFE project")
+|> ignore
+
+storage.AddTodo(Todo.create "Write your app")
+|> ignore
+
+storage.AddTodo(Todo.create "Ship it !!!")
+|> ignore
+
+let todosApi =
+    { getTodos = fun () -> async { return storage.GetTodos() }
+      addTodo =
+          fun todo ->
+              async {
+                  match storage.AddTodo todo with
+                  | Ok () -> return todo
+                  | Error e -> return failwith e
+              } }
+
+let webApp =
+    Remoting.createApi ()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.fromValue todosApi
+    |> Remoting.buildHttpHandler
 
 // https://devcenter.heroku.com/articles/container-registry-and-runtime#dockerfile-commands-and-runtime
 let port =
     System.Environment.GetEnvironmentVariable "PORT"
     |> function null -> "8085" | p -> p
 
-[<EntryPoint>]
-let main _ =
-    Host.CreateDefaultBuilder()
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .UseStartup(typeof<Startup.Startup>)
-                    .UseWebRoot("public")
-                    .UseUrls [| "http://0.0.0.0:" + port |]
-                    |> ignore)
-        .Build()
-        .Run()
-    0
+let app =
+    application {
+        url ("http://0.0.0.0:" + port)
+        use_router webApp
+        memory_cache
+        use_static "public"
+        use_gzip
+    }
+
+run app
